1eb37dfdb5568d9c353f8d11f7ab4d532840305a
admin <ahmed.g@live.com>
2023-05-02 00:21:33 +0300

    new

---
+++ app/src/main/java/com/example/campusdelivery/Server.java
@@ -0,0 +1,6 @@
+package com.example.campusdelivery;
+
+public class Server {
+    public static String ip ="http://10.0.2.2/campus_delivery/";
+   // public static String ip ="http://192.168.1.227/campus_delivery/";
+}
